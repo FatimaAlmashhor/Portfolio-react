daa128442f3766b78c4e45a3130bc1233a24bd2c
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-01-10 21:14:30 +0300

    Updates

---
+++ precache-manifest.e745a3188a399c7d0fb18dc7ab022c5b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646e3a1f45413c9a4ee7cc1a5e7117b6",
+    "url": "/Portfolio-react/index.html"
+  },
+  {
+    "revision": "c9d252017b3242817e16",
+    "url": "/Portfolio-react/static/css/main.4bc69e8d.chunk.css"
+  },
+  {
+    "revision": "140054a231ac0c0eb284",
+    "url": "/Portfolio-react/static/js/2.927cae39.chunk.js"
+  },
+  {
+    "revision": "739d75e7b081c99083237b1b73d68faf",
+    "url": "/Portfolio-react/static/js/2.927cae39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9d252017b3242817e16",
+    "url": "/Portfolio-react/static/js/main.4d4cc4f5.chunk.js"
+  },
+  {
+    "revision": "db05d98eee41f8d20167",
+    "url": "/Portfolio-react/static/js/runtime-main.c002b0fd.js"
+  },
+  {
+    "revision": "f8f7dd2ae4aafe6a03ed0e2d6165f8c6",
+    "url": "/Portfolio-react/static/media/covid.f8f7dd2a.png"
+  },
+  {
+    "revision": "977299887373cf7a47c3ef0485d1fc17",
+    "url": "/Portfolio-react/static/media/ecommerce-fram.97729988.png"
+  },
+  {
+    "revision": "64193052c6ab22914742b46f9de03b21",
+    "url": "/Portfolio-react/static/media/exhale.64193052.png"
+  },
+  {
+    "revision": "ff568f42bb424be53eef3ab21138dd75",
+    "url": "/Portfolio-react/static/media/hange-man.ff568f42.png"
+  },
+  {
+    "revision": "bf976b02e89da80c9d88e930a88ade37",
+    "url": "/Portfolio-react/static/media/hello.bf976b02.svg"
+  }
+]);
